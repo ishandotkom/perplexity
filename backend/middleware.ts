90ecde5fad5c12e87a1e7f3cc26dca277df5ccf4
Mursaleen <mohdmursaleen9306@gmail.com>
2026-05-01 17:03:05 +0530

    feat: implement auth pages, dashboard UI, and backend integration for query handling

--- backend/middleware.ts
+++ backend/middleware.ts
@@ -14,7 +14,10 @@ export async function authMiddleware(
   const userId = data.data.user?.id;
   if (userId) {
     try {
-      const provider = data.data.user?.app_metadata.provider === 'google' ? 'GOOGLE' : 'GITHUB';
+      const provider =
+        data.data.user?.app_metadata.provider === "google"
+          ? "GOOGLE"
+          : "GITHUB";
       // Use upsert to avoid throwing errors if the user already exists
       await prisma.user.upsert({
         where: { id: data.data.user?.id },
@@ -25,16 +28,16 @@ export async function authMiddleware(
           email: data.data.user?.email!,
           provider: provider,
           name: data.data.user?.user_metadata.full_name,
-        }
+        },
       });
-    } catch(e) {
+    } catch (e) {
       console.error("Error creating/updating user in DB:", e);
     }
     req.userId = userId;
     next();
   } else {
     res.status(403).json({
-      error: "Not Authorized"
-    })
+      error: "Not Authorized",
+    });
   }
 }
